aa81d0f25bef486ab061f16c97ca6529ff26db05
jeikar1 <lucasmedinamengo@gmail.com>
2025-06-05 19:07:01 -0300

    Update movimento_peças.c

--- movimento_peças.c
+++ movimento_peças.c
@@ -30,6 +30,24 @@ int main() {
         printf("Direita\n");
     }
 
+    int movimentoscavalo = 1;
+    int passosbaixo = 2;    // duas casas para baixo
+    int passosesquerda = 1; // uma casa para a esquerda
+   
+    printf("Movimento do Cavalo:\n");
+
+    for (int m = 0; m < movimentoscavalo; m++) {
+        
+        int passo = 0;         
+        while (passo < passosbaixo) {  // duas casas para baixo
+            printf("Baixo\n");
+            passo++;
+        }
+
+        for (int k = 0; k < passosesquerda; k++) {  // uma casa para a esquerda
+            printf("Esquerda\n");
+        }
+    }
 
     return 0;
 }
